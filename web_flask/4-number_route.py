4ec870f9a15bfc66b09561b0e6a49dd8b03711ef
Seamas007 <olufy007@gmail.com>
2023-04-25 04:25:09 +0000

    Task 4

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""
-    Starts a Flask web application.
-"""
+"""Module for a Flask web application"""
 
 from flask import Flask
 
@@ -9,76 +7,34 @@ app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def index():
-    """ Displays the index page """
-    return 'Hello HBNB!'
+def hello_hbnb():
+    """Displays "Hello HBNB!" when accessing root"""
+    return "Hello HBNB!"
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """ Displays the hbnb page """
-    return 'HBNB'
+    """Displays "HBNB" when accessing /hbnb"""
+    return "HBNB"
 
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
-    """ Displays the c page """
-    return 'C {}'.format(text.replace('_', ' '))
+    """Displays "C" followed by the value of the text variable"""
+    return "C {}".format(text.replace("_", " "))
 
 
 @app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python_text(text):
-    """ Displays the python page """
-    return 'Python {}'.format(text.replace('_', ' '))
+    """Displays "Python" followed by the value of the text variable"""
+    return "Python {}".format(text.replace("_", " "))
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
 def number(n):
-    """ Displays the number page """
-    return '{} is a number'.format(n)
-
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)#!/usr/bin/python3
-"""
-    Starts a Flask web application.
-"""
-
-from flask import Flask
-
-app = Flask(__name__)
-
-
-@app.route('/', strict_slashes=False)
-def index():
-    """ Displays the index page """
-    return 'Hello HBNB!'
-
-
-@app.route('/hbnb', strict_slashes=False)
-def hbnb():
-    """ Displays the hbnb page """
-    return 'HBNB'
-
-
-@app.route('/c/<text>', strict_slashes=False)
-def c_text(text):
-    """ Displays the c page """
-    return 'C {}'.format(text.replace('_', ' '))
-
-
-@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
-@app.route('/python/<text>', strict_slashes=False)
-def python_text(text):
-    """ Displays the python page """
-    return 'Python {}'.format(text.replace('_', ' '))
-
-
-@app.route('/number/<int:n>', strict_slashes=False)
-def number(n):
-    """ Displays the number page """
-    return '{} is a number'.format(n)
+    """Displays "<n> is a number" only if n is an integer"""
+    return "{} is a number".format(n)
 
 
 if __name__ == '__main__':
