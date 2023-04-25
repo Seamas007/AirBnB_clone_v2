11e63373bae1bbab437c3bbb58ff36475c296547
Seamas007 <olufy007@gmail.com>
2023-04-25 04:44:07 +0000

    Task 6

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -1,56 +1,107 @@
 #!/usr/bin/python3
-""" Starts a Flask web application """
+"""Starts a Flask web application"""
 
 from flask import Flask, render_template
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
-@app.route('/', strict_slashes=False)
+@app.route('/')
 def hello_hbnb():
-    """ Displays "Hello HBNB!" """
+    """Displays 'Hello HBNB!'"""
     return 'Hello HBNB!'
 
 
-@app.route('/hbnb', strict_slashes=False)
+@app.route('/hbnb')
 def hbnb():
-    """ Displays "HBNB" """
+    """Displays 'HBNB'"""
     return 'HBNB'
 
 
-@app.route('/c/<text>', strict_slashes=False)
-def c_is_fun(text):
-    """ Displays "C <text>", with text variable replaced with underscores """
+@app.route('/c/<text>')
+def c(text):
+    """Displays 'C' followed by the value of the text variable"""
     return 'C {}'.format(text.replace('_', ' '))
 
 
-@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
-@app.route('/python/<text>', strict_slashes=False)
-def python_is_cool(text):
-    """ Displays "Python <text>", with text variable replaced with underscores """
+@app.route('/python/')
+@app.route('/python/<text>')
+def python(text='is cool'):
+    """Displays 'Python' followed by the value of the text variable"""
     return 'Python {}'.format(text.replace('_', ' '))
 
 
-@app.route('/number/<int:n>', strict_slashes=False)
-def is_number(n):
-    """ Displays "<n> is a number" only if n is an integer """
+@app.route('/number/<int:n>')
+def number(n):
+    """Displays 'n is a number' only if n is an integer"""
     return '{} is a number'.format(n)
 
 
-@app.route('/number_template/<int:n>', strict_slashes=False)
+@app.route('/number_template/<int:n>')
 def number_template(n):
-    """ Displays a HTML page only if n is an integer: H1 tag: “Number:
- n” inside the tag BODY """
+    """Displays a HTML page only if n is an integer"""
     return render_template('5-number.html', n=n)
 
 
-@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+@app.route('/number_odd_or_even/<int:n>')
 def number_odd_or_even(n):
-    """ Displays a HTML page only if n is an integer: H1 tag: “Number: n is even|odd” 
-inside the tag BODY """
+    """Displays a HTML page only if n is an integer"""
+    return render_template('6-number_odd_or_even.html', n=n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)#!/usr/bin/python3
+"""Starts a Flask web application"""
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello_hbnb():
+    """Displays 'Hello HBNB!'"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb():
+    """Displays 'HBNB'"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def c(text):
+    """Displays 'C' followed by the value of the text variable"""
+    return 'C {}'.format(text.replace('_', ' '))
+
+
+@app.route('/python/')
+@app.route('/python/<text>')
+def python(text='is cool'):
+    """Displays 'Python' followed by the value of the text variable"""
+    return 'Python {}'.format(text.replace('_', ' '))
+
+
+@app.route('/number/<int:n>')
+def number(n):
+    """Displays 'n is a number' only if n is an integer"""
+    return '{} is a number'.format(n)
+
+
+@app.route('/number_template/<int:n>')
+def number_template(n):
+    """Displays a HTML page only if n is an integer"""
+    return render_template('5-number.html', n=n)
+
+
+@app.route('/number_odd_or_even/<int:n>')
+def number_odd_or_even(n):
+    """Displays a HTML page only if n is an integer"""
     return render_template('6-number_odd_or_even.html', n=n)
 
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-
